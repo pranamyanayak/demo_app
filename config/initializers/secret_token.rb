694d1819d3eab40618d7715984611ebe7e5bc34a
Pranamya <pnayak@groupon.com>
2012-12-11 11:03:36 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '80fe72fd4711344980d28bdfdf8a6b6b26d39b22c79ad0cc61d78ce424af8192a8d07e34737035c33d85938c90e8488b495f41a85bb239e26e1d28d7ffd32e94'
